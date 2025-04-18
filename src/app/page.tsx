ef8b190394900a5b0bd91a198da08934ecf7d88d
lennypruss <lennypruss@gmail.com>
2025-04-18 08:53:38 -0700

    Remove unused Image import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import Image from "next/image";
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(true);
